4193fc072e48f6532c28cc72b13585e31826374a
oschildt <oleg.schildt@gmail.com>
2018-05-05 23:46:14 +0300

    Code improvements due to PHPStorm

--- application/classes/MyApplication/UserSettingsValidator.php
+++ application/classes/MyApplication/UserSettingsValidator.php
@@ -43,4 +43,3 @@ class UserSettingsValidator implements ISettingsValidator
   //-----------------------------------------------------------------
 } // UserSettingsValidator
 //-------------------------------------------------------------------
-?>
